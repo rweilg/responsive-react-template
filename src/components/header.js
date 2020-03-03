f658c5bfc117e2013728a0f5ed9a972f221261f6
Rodrigo Weilg <contact@rodrigoweilg.com>
2020-03-03 22:16:30 +0100

    removed rel=noreopener

--- src/components/header.js
+++ src/components/header.js
@@ -1,55 +1,39 @@
 import React from "react"
 import { Link } from "gatsby"
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      hasOpened: false,
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener("click", this.hasOpened)
-  }
-
-  render() {
-    return (
-      <header>
-        <div className="container" id="contenitore">
-          <div className="semi-inner">
-            <div className="inner-header">
-              <div className="logo">
-                <Link style={{ marginLeft: "0px" }} to="/">
-                  {" "}
-                  Your Name
+const Header = () => (
+  <header>
+    <div className="container" id="contenitore">
+      <div className="semi-inner">
+        <div className="inner-header">
+          <div className="logo">
+            <Link style={{ marginLeft: "0px" }} to="/">
+              {" "}
+              Your Name
+            </Link>
+          </div>
+          <div className="header-tranne-logo">
+            <div className="navigation" id="elementi-menu">
+              <nav>
+                <Link className="menu" to="/page1">
+                  Page 1
                 </Link>
-              </div>
-              <div className="header-tranne-logo">
-                <div className="navigation" id="elementi-menu">
-                  <nav>
-                    <Link className="menu" to="/page1">
-                      Page 1
-                    </Link>
-                    <Link className="menu" to="/page2">
-                      Page 2
-                    </Link>
-                    <Link className="menu" to="/page3">
-                      Page 3
-                    </Link>
-                    <Link className="menu" to="/page4">
-                      Page 4
-                    </Link>
-                  </nav>
-                </div>
-              </div>
+                <Link className="menu" to="/page2">
+                  Page 2
+                </Link>
+                <Link className="menu" to="/page3">
+                  Page 3
+                </Link>
+                <Link className="menu" to="/page4">
+                  Page 4
+                </Link>
+              </nav>
             </div>
           </div>
         </div>
-      </header>
-    )
-  }
-}
+      </div>
+    </div>
+  </header>
+)
 
 export default Header
