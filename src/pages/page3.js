398b5a1256e211784d6aec95a73d2c3e7d2f362a
Rodrigo Weilg <contact@rodrigoweilg.com>
2020-03-03 21:54:26 +0100

    put content in pages

--- src/pages/page3.js
+++ src/pages/page3.js
@@ -16,7 +16,46 @@ const Page3 = () => (
       <Header />
       <SideMenu />
     </div>
-    <h1>Page 3</h1>
+    <div className="page-content" style={{ marginTop: "68px" }}>
+      <div className="main-text">
+        <h1>Page 3 Title</h1>
+        <h3>March 3rd 2020</h3>
+        <h2>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel
+          dolor sed arcu elementum ornare.
+        </h2>
+        <ImageLoader Photo={require("../images/octiheadline.png")} />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel
+          dolor sed arcu elementum ornare. Mauris malesuada mauris in arcu
+          volutpat, in commodo augue auctor. Sed hendrerit nisl ut turpis
+          sodales tristique. Vivamus pulvinar in lectus id faucibus. Suspendisse
+          iaculis facilisis laoreet. Vivamus eu pulvinar velit. In pulvinar nunc
+          eu libero lacinia, at congue justo mollis. Phasellus ultricies magna
+          et turpis molestie, et congue ex vehicula. Sed eget elit urna. Integer
+          aliquet quis nulla sed sodales.
+          <br />
+          <h4>Lorem ipsum again</h4>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel
+          dolor sed arcu elementum ornare. Mauris malesuada mauris in arcu
+          volutpat, in commodo augue auctor. Sed hendrerit nisl ut turpis
+          sodales tristique. Vivamus pulvinar in lectus id faucibus. Suspendisse
+          iaculis facilisis laoreet. Vivamus eu pulvinar velit. In pulvinar nunc
+          eu libero lacinia, at congue justo mollis. Phasellus ultricies magna
+          et turpis molestie, et congue ex vehicula. Sed eget elit urna. Integer
+          aliquet quis nulla sed sodales.
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel
+          dolor sed arcu elementum ornare. Mauris malesuada mauris in arcu
+          volutpat, in commodo augue auctor. Sed hendrerit nisl ut turpis
+          sodales tristique. Vivamus pulvinar in lectus id faucibus. Suspendisse
+          iaculis facilisis laoreet. Vivamus eu pulvinar velit. In pulvinar nunc
+          eu libero lacinia, at congue justo mollis. Phasellus ultricies magna
+          et turpis molestie, et congue ex vehicula. Sed eget elit urna. Integer
+          aliquet quis nulla sed sodales.
+        </p>
+      </div>
+    </div>
   </div>
 )
 
